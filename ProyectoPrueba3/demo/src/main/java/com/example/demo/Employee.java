1ad402aa63b8961720c35a751d06d0ddc22ae865
mikels111 <idpracticas@dinycon.es>
2021-11-23 09:47:15 +0100

    23/11/21

---
+++ ProyectoPrueba3/demo/src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    private String id;
+    private String name;
+    public Employee(String id, String name){
+        this.id=id;
+        this.name=name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
